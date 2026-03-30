2531e47fdc4a00ead7dd4bb2c0400815a18a6919
irene <irene_romero_95@hotmail.com>
2026-03-30 16:50:48 +0200

    feat: add button

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('JS is connected! 🚀');
+
+const toggleButton = document.getElementById('theme-toggle');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+  if (body.getAttribute('data-theme') === 'dark') {
+    body.removeAttribute('data-theme');
+  } else {
+    body.setAttribute('data-theme', 'dark');
+  }
+
+  toggleButton.textContent =
+    body.getAttribute('data-theme') === 'dark'
+      ? '🌞 Light Mode'
+      : '🌙 Dark Mode';
+});
